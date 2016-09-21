d9ca7ef475c90087b19b5e3c174ab951367daa73
Gary Ditsch <garyditsch@gmail.com>
2016-09-21 11:59:20 -0400

    adding working bookshelves render

--- bookcases/views.py
+++ bookcases/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db.models import Count
+from .models import Bookcase
 
-# Create your views here.
+def bookcase_list(request):
+    bookcases = Bookcase.objects.annotate(self_count=Count('bookshelf')).all()
+
+    context = {
+        "bookcases": bookcases
+    }
+
+    return render(request, "bookcases/bookcase_list.html", context)
--- bookworm/urls.py
+++ bookworm/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^bookcases/', include('bookcases.urls', namespace='bookcases')),
     url(r'^', include('core.urls', namespace='core')),
 ]
